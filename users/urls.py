754bc10e089c8e1d79ee953a9c2f3bea5730e72e
nonwaves <84790075+nonwaves@users.noreply.github.com>
2021-10-06 12:08:45 -0400

    Update 10.6.21
    Got 'users' page to display a list of user names from the "profiles" database module.

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-from .views import UserListView
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.UserListView.as_view(), name='user_list'),
+    path('', views.profiles, name='profiles'),
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,23 +1,10 @@
-from django.db.models.query_utils import select_related_descend
 from django.shortcuts import render
-from django.contrib.auth import get_user_model
-from accounts.models import User
-from django.views.generic import ListView
+from .models import Profile
 
 # Create your views here.
 
-User = get_user_model()
-
 def profiles(request):
-    return render(request, 'users/profiles.html')
-
-class UserListView(ListView):
-    model = User
-    template_name = 'users/user_list.html'
-    
-    
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        return queryset.filter(user__username__iexact=self.kwargs.get('username'))
-    
+    profiles = Profile.objects.all()
+    context = {'profiles':profiles}
+    return render(request, 'users/profile_list.html', context)
 
